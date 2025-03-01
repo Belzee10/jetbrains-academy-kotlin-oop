770a001fa1a5fd1f077e9e31e37e5662fe646527
Ivan Perez Abreu <belzee92@gmail.com>
2025-03-01 16:58:20 +0100

    complete Words Generator exercise

--- wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/results/GameResultsService.kt
+++ wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/results/GameResultsService.kt
@@ -1,10 +1,28 @@
 package jetbrains.kotlin.course.words.generator.results
 
+import jetbrains.kotlin.course.words.generator.team.Team
+import jetbrains.kotlin.course.words.generator.team.TeamService
 import org.springframework.stereotype.Service
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+    companion object {
+        val gameHistory: MutableList<GameResult> = mutableListOf()
+    }
+
+    fun saveGameResults(result: GameResult): Unit {
+        if (result.isEmpty()) error("The result is empty")
+
+        if(!result.all { it.id in TeamService.teamsStorage }) error("The result ids must be present in teamsStorage")
+
+        gameHistory.add(result)
+    }
+
+    fun getAllGameResults(): List<GameResult> = gameHistory.reversed()
 }
+
+
+
--- wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/word/WordService.kt
+++ wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/word/WordService.kt
@@ -1,12 +1,42 @@
 package jetbrains.kotlin.course.words.generator.word
 
+import jetbrains.kotlin.course.words.generator.util.words
 import org.springframework.stereotype.Service
 
 @Service
 class WordService {
-    fun generateNextWord(): Word = TODO("Not implemented yet")
+    companion object {
+        val numberOfWords: Int = words.size
+        val previousWords: MutableMap<String, MutableList<Word>> = mutableMapOf()
+    }
 
-    fun isValidWord(keyWord: String, newWord: String): Boolean = TODO("Not implemented yet")
+    fun generateNextWord(): Word {
+        if (words.isEmpty()) error("Words list cannot be empty")
 
-    fun isNewWord(keyWord: String, newWord: String): Boolean = TODO("Not implemented yet")
+        return Word(words.removeFirst())
+    }
+
+    fun isValidWord(keyWord: String, newWord: String): Boolean {
+        if (newWord.isEmpty()) return false
+
+        val newWordGrouped = newWord.groupingBy { it }.eachCount()
+        val keyWordGrouped = keyWord.groupingBy { it }.eachCount()
+
+        if(keyWordGrouped.any { (newWordGrouped[it.key] ?: 0) > it.value }) return false
+
+        if (!keyWord.contains(newWord)) return false
+
+        return true
+    }
+
+    fun isNewWord(keyWord: String, newWord: String): Boolean {
+        if (!previousWords.contains(keyWord)) {
+            previousWords.putIfAbsent(keyWord, mutableListOf(Word(newWord)))
+            return true
+        }
+
+        if ((previousWords[keyWord])!!.contains(Word(newWord))) return false
+        previousWords[keyWord]?.add(Word(newWord))
+        return true
+    }
 }
