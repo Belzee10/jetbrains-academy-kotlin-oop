e1e2b18261e9fff5fd2d9b2bb7272f2aefb8e6fb
Ivan Perez Abreu <belzee92@gmail.com>
2025-03-05 14:46:45 +0100

    complete Codenames exercise

--- codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardService.kt
+++ codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardService.kt
@@ -1,8 +1,20 @@
 package jetbrains.kotlin.course.codenames.card
 
+import jetbrains.kotlin.course.codenames.utils.Utils
+import jetbrains.kotlin.course.codenames.utils.words
 import org.springframework.stereotype.Service
 
 @Service
 class CardService {
-    fun generateWordsCards(): List<Card> = TODO("Not implemented yet")
+    fun generateWordsCards(): List<Card> {
+        require(words.size >= Utils.TOTAL_NUMBER) { "TOTAL_NUMBER is bigger than the amount of words" }
+
+        val shuffledWords = words.shuffled()
+
+        val wordsList = shuffledWords.take(Utils.TOTAL_NUMBER)
+
+        words = shuffledWords.drop(Utils.TOTAL_NUMBER)
+
+        return wordsList.map { Card(WordCardData(it), CardState.Front) }
+    }
 }
--- codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/utils/Utils.kt
+++ codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/utils/Utils.kt
@@ -1,1 +1,45 @@
 package jetbrains.kotlin.course.codenames.utils
+
+import jetbrains.kotlin.course.codenames.keyCard.KeyCardCell
+import jetbrains.kotlin.course.codenames.keyCard.KeyCardType
+
+
+
+object Utils {
+    private const val N = 5
+    const val TOTAL_NUMBER = N * N
+    const val PINK_CARDS_NUMBER = 8
+    const val VIOLET_CARDS_NUMBER = 9
+    const val GRAY_CARDS_NUMBER = 7
+    const val BLACK_CARDS_NUMBER = 1
+
+    private val previousAttempts: MutableList<List<KeyCardCell>> = mutableListOf()
+
+    val uniqueKeyCardGenerator = KeyCardGenerator {
+        var newKeyCard: List<KeyCardCell>
+
+        do {
+            newKeyCard = KeyCardType.values()
+                .flatMap { type -> List(type.number) { KeyCardCell(type) } }
+                .shuffled()
+        } while (previousAttempts.contains(newKeyCard))
+
+        previousAttempts.add(newKeyCard)
+        newKeyCard
+    }
+
+    init {
+        val sum = PINK_CARDS_NUMBER + VIOLET_CARDS_NUMBER + GRAY_CARDS_NUMBER + BLACK_CARDS_NUMBER
+        require(sum == TOTAL_NUMBER) { "The total number of cards in the game must be: $TOTAL_NUMBER" }
+    }
+}
+
+fun interface KeyCardGenerator {
+    fun generateData(): List<KeyCardCell>
+}
+
+
+
+
+
+
